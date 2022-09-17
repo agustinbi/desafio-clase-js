a53421e864da883fb08bdabe92093e7045f652ff
AgustinJ <78747851+agustinbi@users.noreply.github.com>
2022-09-17 16:38:24 -0300

    Update desafio-clase.js

--- desafio-clase.js
+++ desafio-clase.js
@@ -5,12 +5,49 @@ class Usuario{
         this.libros = libros;
         this.mascotas = mascotas;
     }
-    addBook(nombre,autor){
+    addBook(nombre, autor){
         const newBook = {nombre: nombre, autor: autor};
-        this.libros.push(newBook);
+        console.log(newBook);
+        this.libros.push(newBook)
+        
+    }
+    
+    addMascota(nombre) {
+    const newMascota = {nombre: nombre};
+    console.log(newMascota);
+    this.mascotas.push(newMascota);
+    }
+    countMascotas(){
+    const contar = this.mascotas.length
+    console.log(contar)
+    
+    }
+    
+    getFullName(){
+    console.log('mi nombre es ${this.nombre} ${this.apellido}')
+    
+    }
+      getBookNames(){
+      let coleccion = this.libros
+      let nombre = [];
+          coleccion.map(libros=>{
+          nombre.push(libro.nombre)
+     })
+          
+          console.log(nombre)
+    
+                        
+                        
+    }
+    
     }
 
-}
+const usuarioUno = new Usuario("Agustin","Julian",[{nombre:"It", autor:"sthepen king"}],["ricky","adolf"])
 
-const Usuario1 = new Usuario("Agustin","Julian",[{nombre:"It", autor:"sthepen king"}],["tony","lucas"])
-Usuario1.addBook("El hobbit","J.R tolkien");
+console.log(usuarioUno)
+usuarioUno.addBook("Orgullo y Prejuicio","Jane Austen")
+usuarioUno.addMascota("monina")
+usuarioUno.countMascotas()
+usuarioUno.getFullName()
+usuarioUno.getBookNames()
+
